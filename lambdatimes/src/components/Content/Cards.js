cc1b6fefd3befd6a848309d9c88f39d04b0996be
McTrip777 <mcjakedude@gmail.com>
2019-02-22 12:35:59 -0800

    sprint commit Re-attempt MVP

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
 
 const Cards = props => {
@@ -15,4 +16,14 @@ const Cards = props => {
 }
 
 // Make sure you include prop types for all of your incoming props
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(
+    PropTypes.shape({
+    headline: PropTypes.string,
+    img: PropTypes.string,
+    author: PropTypes.string
+    })
+  )
+ }
+ 
 export default Cards;
